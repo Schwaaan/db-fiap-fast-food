fbfa9e434596068dca06eb9e40737d88b46350b4
Jair Schwan <jair.schwan9@gmail.com>
2024-03-17 16:04:58 -0300

    update terraform

--- main.tf
+++ main.tf
@@ -6,14 +6,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_db_instance" "FourSix" {
+resource "aws_db_instance" "fourSixInstance" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "sqlserver-se"
   engine_version       = "15.00.2070.41.v1"
   instance_class       = "db.t3.micro"
   name                 = "FourSix"
-  identifier          = "FourSix"
   username             = "sa"
   password             = "SenhaDoBanco(123)"
 }
