993d1f1003eab4b2d83db4ff6d9e47787431c5e4
Jair Schwan <jair.schwan9@gmail.com>
2024-03-17 15:49:41 -0300

    update terraform

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "FourSix" {
   storage_type         = "gp2"
   engine               = "sqlserver-se"
   engine_version       = "15.00.2070.41.v1"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
   name                 = "FourSix"
   username             = "sa"
   password             = "SenhaDoBanco(123)"
